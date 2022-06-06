f10c2dad85025aeb7e6c697843bb9ac898ad1a90
Ayodele Fagbami <afagbami@gmail.com>
2022-06-06 10:28:14 +0100

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'features/chatroom/presentation/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,8 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Clean Architecture Responsive Chat room',
-      home: HomeScreen()
-    );
+        title: 'Clean Architecture Responsive Chat room', home: HomeScreen());
   }
 }
