5528a630c6b8a672f483d126b52a2f4830290e86
Ayodele Fagbami <afagbami@gmail.com>
2022-06-06 09:47:40 +0100

    add const constructor

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'features/chatroom/presentation/screens/home_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
