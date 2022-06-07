7177f03e49962a125f28b6da0c74987ef87973e2
Ayodele Fagbami <afagbami@gmail.com>
2022-06-07 10:56:42 +0100

    created responsive pages

---
+++ lib/features/chatroom/presentation/pages/tablet/tablet_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TabletPage extends StatelessWidget {
+  const TabletPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
