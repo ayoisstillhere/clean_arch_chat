575f0c2d1f7ea8d6d9cfdab370affb25fc98fa28
Ayodele Fagbami <afagbami@gmail.com>
2022-06-07 13:59:18 +0100

    done web page UI

--- lib/features/chatroom/presentation/pages/web/web_page.dart
+++ lib/features/chatroom/presentation/pages/web/web_page.dart
@@ -1,10 +1,26 @@
+import 'package:clean_arch_chat/features/chatroom/presentation/pages/web/widgets/left_side_widget.dart';
+import 'package:clean_arch_chat/features/chatroom/presentation/pages/web/widgets/right_side_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class WebPage extends StatelessWidget {
-  const WebPage({Key? key}) : super(key: key);
+  final SizingInformation sizingInformation;
+  const WebPage({
+    Key? key,
+    required this.sizingInformation,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Row(
+          children: [
+            LeftSideWidget(sizingInformation: sizingInformation),
+            Expanded(child: RightSideWidget(sizingInformation: sizingInformation))
+          ],
+        ),
+      ),
+    );
   }
 }
