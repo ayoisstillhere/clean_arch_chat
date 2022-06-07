69c50c743f8d100bdc4e131f99d6eee7f462d44e
Ayodele Fagbami <afagbami@gmail.com>
2022-06-07 10:57:32 +0100

    responsive page imports

--- lib/features/chatroom/presentation/screens/home_screen.dart
+++ lib/features/chatroom/presentation/screens/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
+import '../pages/mobile/mobile_page.dart';
+import '../pages/tablet/tablet_page.dart';
+import '../pages/web/web_page.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
