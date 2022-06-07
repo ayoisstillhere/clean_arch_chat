988477ea286edcdb23147399c1af4cb09e7c6064
Ayodele Fagbami <afagbami@gmail.com>
2022-06-07 10:49:32 +0100

    add responsivebuilder

--- lib/features/chatroom/presentation/screens/home_screen.dart
+++ lib/features/chatroom/presentation/screens/home_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("HI"),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        if (sizingInformation.isDesktop) {
+          return WebPage();
+        }
+        if (sizingInformation.isTablet) {
+          return TabletPage();
+        }
+        return MobilePage();
+      },
     );
   }
 }
