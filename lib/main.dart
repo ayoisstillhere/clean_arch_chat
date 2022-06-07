2dbb4f8503328581ddf69b2c5f8e7a7cbce92a8b
Ayodele Fagbami <afagbami@gmail.com>
2022-06-07 10:51:02 +0100

    firebase web options setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'features/chatroom/presentation/screens/home_screen.dart';
@@ -5,7 +6,20 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCvHMw7gvNo9qbaG3O7flGoIZnbCBhiP6M",
+        appId: "1:1063131070191:web:66e1e522e7868ccb6d651a",
+        messagingSenderId: "1063131070191",
+        projectId: "cleanarchigroupchat",
+        storageBucket: "cleanarchigroupchat.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -15,6 +29,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Clean Architecture Responsive Chat room', home: HomeScreen());
+      debugShowCheckedModeBanner: false,
+      title: 'Clean Architecture Responsive Chat room',
+      home: HomeScreen(),
+    );
   }
 }
