832ed829bfef18b3dd0cb80ce935d35c93aed8e8
Ayodele Fagbami <afagbami@gmail.com>
2022-06-08 16:04:08 +0100

    implement repo with remote data source

--- lib/features/chatroom/data/repositories/firebase_repository_impl.dart
+++ lib/features/chatroom/data/repositories/firebase_repository_impl.dart
@@ -1,28 +1,30 @@
+import 'package:clean_arch_chat/features/chatroom/data/datasources/firebase_remote_datasource.dart';
+
 import '../../domain/repositories/firebase_repository.dart';
 
 class FirebaseRepositoryImpl implements FirebaseRepository {
+  final FirebaseRemoteDataSource firebaseRemoteDataSource;
+
+  FirebaseRepositoryImpl({required this.firebaseRemoteDataSource});
   @override
-  Future<String> getCurrentUid() {
-    // TODO: implement getCurrentUid
-    throw UnimplementedError();
-  }
+  Future<String> getCurrentUid() async =>
+      await firebaseRemoteDataSource.getCurrentUid();
 
   @override
-  Future<bool> isSignIn() {
-    // TODO: implement isSignIn
-    throw UnimplementedError();
-  }
+  Future<bool> isSignIn() async => await firebaseRemoteDataSource.isSignIn();
 
   @override
-  Future<void> signIn(String email, String password) {
-    // TODO: implement signIn
-    throw UnimplementedError();
-  }
+  Future<void> signIn(String email, String password) async =>
+      await firebaseRemoteDataSource.signIn(email, password);
 
   @override
-  Future<void> signUp(String email, String password) {
-    // TODO: implement signUp
-    throw UnimplementedError();
-  }
+  Future<void> signUp(String email, String password) async =>
+      await firebaseRemoteDataSource.signUp(email, password);
 
+  @override
+  Future<void> getCreateCurrentUser(
+      String email, String name, String profileUrl, String uid) async {
+    return await firebaseRemoteDataSource.getCreateCurrentUser(
+        email, name, profileUrl, uid);
+  }
 }
