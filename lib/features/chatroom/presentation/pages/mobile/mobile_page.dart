83948a80e294f6e91a28ec429acf860a342ac461
Ayodele Fagbami <afagbami@gmail.com>
2022-06-08 11:01:15 +0100

    finished mobile ui

--- lib/features/chatroom/presentation/pages/mobile/mobile_page.dart
+++ lib/features/chatroom/presentation/pages/mobile/mobile_page.dart
@@ -1,12 +1,20 @@
+import 'package:clean_arch_chat/features/chatroom/presentation/pages/mobile/widgets/body_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class MobilePage extends StatelessWidget {
   const MobilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("yet to do")),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        return Scaffold(
+          body: Container(
+            child: BodyWidget(sizingInformation: sizingInformation),
+          ),
+        );
+      },
     );
   }
 }
