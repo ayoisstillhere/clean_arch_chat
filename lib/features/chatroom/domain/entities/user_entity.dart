f34e68dee17281bb4b4f8f74b86654a2e39c217a
Ayodele Fagbami <afagbami@gmail.com>
2022-06-08 14:50:57 +0100

    changed name  to UserEntity

--- lib/features/chatroom/domain/entities/user_entity.dart
+++ lib/features/chatroom/domain/entities/user_entity.dart
@@ -1,12 +1,12 @@
 import 'package:equatable/equatable.dart';
 
-class User extends Equatable{
+class UserEntity extends Equatable{
   final String name;
   final String email;
   final String uid;
   final String profileUrl;
 
-  User(this.name, this.email, this.uid, this.profileUrl);
+  UserEntity(this.name, this.email, this.uid, this.profileUrl);
   
   @override
   List<Object?> get props => [name, email, uid, profileUrl];
