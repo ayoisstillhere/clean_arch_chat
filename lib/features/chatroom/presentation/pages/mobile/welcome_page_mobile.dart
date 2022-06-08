b4089f629c9214b2d4d26f3f49448a882ebe2ba6
Ayodele Fagbami <afagbami@gmail.com>
2022-06-08 20:39:41 +0100

    created and added welcome pages for each device

---
+++ lib/features/chatroom/presentation/pages/mobile/welcome_page_mobile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomePageMobile extends StatelessWidget {
+  const WelcomePageMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/features/chatroom/presentation/screens/welcome_screen.dart
+++ lib/features/chatroom/presentation/screens/welcome_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+import '../pages/mobile/welcome_page_mobile.dart';
+import '../pages/tablet/welcome_page_tablet.dart';
+import '../pages/web/welcome_page_web.dart';
 
 class WelcomeScreen extends StatelessWidget {
   final String uid;
@@ -6,6 +11,16 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        if (sizingInformation.isDesktop) {
+          return WelcomePageWeb();
+        }
+        if (sizingInformation.isTablet) {
+          return WelcomePageTablet();
+        }
+        return WelcomePageMobile();
+      },
+    );
   }
 }
