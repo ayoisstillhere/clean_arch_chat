395632c9af57dd35708861dd1b440f2c6f1f26ae
Ayodele Fagbami <afagbami@gmail.com>
2022-06-08 10:43:35 +0100

    Finished Tablet UI

--- lib/features/chatroom/presentation/pages/tablet/tablet_page.dart
+++ lib/features/chatroom/presentation/pages/tablet/tablet_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+import 'widgets/left_side_widget.dart';
+import 'widgets/right_side_widget.dart';
 
 class TabletPage extends StatelessWidget {
   const TabletPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        return Scaffold(
+          body: Row(
+            children: [
+              LeftSideWidget(sizingInformation: sizingInformation),
+            Expanded(child: RightSideWidget(sizingInformation: sizingInformation))
+            ],
+          ),
+        );
+      },
+    );
   }
 }
