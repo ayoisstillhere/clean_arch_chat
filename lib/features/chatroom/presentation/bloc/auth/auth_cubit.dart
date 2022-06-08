4afe0c7bbca96926e92c78604058dab57af15639
Ayodele Fagbami <afagbami@gmail.com>
2022-06-08 17:18:48 +0100

    state mgmt

--- lib/features/chatroom/presentation/bloc/auth/auth_cubit.dart
+++ lib/features/chatroom/presentation/bloc/auth/auth_cubit.dart
@@ -1,8 +1,36 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_arch_chat/features/chatroom/domain/usecases/get_current_uid_usecase.dart';
+import 'package:clean_arch_chat/features/chatroom/domain/usecases/is_signin_usecase.dart';
 import 'package:equatable/equatable.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  final IsSignInUseCase isSignInUseCase;
+  final GetCurrentUidUseCase getCurrentUidUseCase;
+  AuthCubit({required this.isSignInUseCase, required this.getCurrentUidUseCase})
+      : super(AuthInitial());
+
+  Future<void> AppStarted() async {
+    try {
+      final isSignIn = await isSignInUseCase.call();
+      if (isSignIn) {
+        final currentUid = await getCurrentUidUseCase.call();
+        emit(Authenticated(uid: currentUid));
+      } else {
+        emit(UnAuthenticated());
+      }
+    } catch (_) {
+      emit(UnAuthenticated());
+    }
+  }
+
+  Future<void> loggedIn() async {
+    final currentUid = await getCurrentUidUseCase.call();
+    emit(Authenticated(uid: currentUid));
+  }
+
+  Future<void> loggedOut() async {
+    emit(UnAuthenticated());
+  }
 }
