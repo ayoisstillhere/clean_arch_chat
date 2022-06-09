b915ce4d3632c8afaa19e9d3e798ab4402ea4610
Ayodele Fagbami <afagbami@gmail.com>
2022-06-09 10:36:13 +0100

    Finished Welcome Pages UI

--- lib/features/chatroom/presentation/pages/tablet/welcome_page_tablet.dart
+++ lib/features/chatroom/presentation/pages/tablet/welcome_page_tablet.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePageTablet extends StatelessWidget {
   const WelcomePageTablet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.indigo[400]!,
+                  Colors.blue[300]!,
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Lottie.asset(
+              "congratulation.json",
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 100),
+              child: Text(
+                "Welcome XYZName",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          _joinGlobalChatButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _joinGlobalChatButton() {
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Join Us For Fun",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Container(
+            width: 250,
+            height: 70,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.3),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              border: Border.all(
+                color: Colors.white60,
+                width: 2,
+              ),
+            ),
+            child: Text(
+              "Join",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/features/chatroom/presentation/pages/web/welcome_page_web.dart
+++ lib/features/chatroom/presentation/pages/web/welcome_page_web.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class WelcomePageWeb extends StatelessWidget {
   const WelcomePageWeb({Key? key}) : super(key: key);
@@ -6,9 +7,75 @@ class WelcomePageWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Text("Welcome")
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.indigo[400]!,
+                  Colors.blue[300]!,
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Lottie.asset(
+              "congratulation.json",
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 150),
+              child: Text(
+                "Welcome XYZName",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          _joinGlobalChatButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _joinGlobalChatButton() {
+    return Align(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Join Us For Fun",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Container(
+            width: 250,
+            height: 70,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.3),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              border: Border.all(
+                color: Colors.white60,
+                width: 2,
+              ),
+            ),
+            child: Text(
+              "Join",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
         ],
       ),
     );
