bde2cd764bfb1d615fb6be80be25bf0392aa62cc
Ayodele Fagbami <afagbami@gmail.com>
2022-06-09 11:42:55 +0100

    Added text methods to firebase repo impl

--- lib/features/chatroom/data/repositories/firebase_repository_impl.dart
+++ lib/features/chatroom/data/repositories/firebase_repository_impl.dart
@@ -1,4 +1,6 @@
 import 'package:clean_arch_chat/features/chatroom/data/datasources/firebase_remote_datasource.dart';
+import 'package:clean_arch_chat/features/chatroom/domain/entities/user_entity.dart';
+import 'package:clean_arch_chat/features/chatroom/domain/entities/text_message_entity.dart';
 
 import '../../domain/repositories/firebase_repository.dart';
 
@@ -26,5 +28,23 @@ class FirebaseRepositoryImpl implements FirebaseRepository {
       String email, String name, String profileUrl) async {
     return await firebaseRemoteDataSource.getCreateCurrentUser(
         email, name, profileUrl,);
+  }
+
+  @override
+  Stream<List<TextMessageEntity>> getMessages() {
+    // TODO: implement getMessages
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<UserEntity>> getUsers() {
+    // TODO: implement getUsers
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendTextMessage(TextMessageEntity textMessage) {
+    // TODO: implement sendTextMessage
+    throw UnimplementedError();
   }
 }
