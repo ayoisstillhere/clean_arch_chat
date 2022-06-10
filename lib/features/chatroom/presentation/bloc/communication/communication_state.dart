8cf2643cd8ae4a56b78bbb8a6609d7dc800d728a
Ayodele Fagbami <afagbami@gmail.com>
2022-06-10 08:52:50 +0100

    Communication Cubit

---
+++ lib/features/chatroom/presentation/bloc/communication/communication_state.dart
@@ -0,0 +1,10 @@
+part of 'communication_cubit.dart';
+
+abstract class CommunicationState extends Equatable {
+  const CommunicationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CommunicationInitial extends CommunicationState {}
