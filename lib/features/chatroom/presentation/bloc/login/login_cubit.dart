190462ea6a1427b45bd5e37ac1940767d01e96c4
Ayodele Fagbami <afagbami@gmail.com>
2022-06-14 16:27:47 +0100

    added sign out usecase to logincubit

--- lib/features/chatroom/presentation/bloc/login/login_cubit.dart
+++ lib/features/chatroom/presentation/bloc/login/login_cubit.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 import 'package:clean_arch_chat/features/chatroom/domain/usecases/get_create_current_user.dart';
+import 'package:clean_arch_chat/features/chatroom/domain/usecases/sign_out_usecase.dart';
 import 'package:clean_arch_chat/features/chatroom/domain/usecases/signin_usecase.dart';
 import 'package:clean_arch_chat/features/chatroom/domain/usecases/signup_usecase.dart';
 
@@ -13,10 +14,12 @@ class LoginCubit extends Cubit<LoginState> {
   final SignUpUseCase signUpUseCase;
   final SignInUseCase signInUseCase;
   final GetCreateCurrentUser getCreateCurrentUser;
+  final SignOutUseCase signOutUseCase;
   LoginCubit({
     required this.signUpUseCase,
     required this.signInUseCase,
     required this.getCreateCurrentUser,
+    required this.signOutUseCase,
   }) : super(LoginInitial());
 
   Future<void> submitLogin({
@@ -47,6 +50,14 @@ class LoginCubit extends Cubit<LoginState> {
       emit(LoginFailure(e.message));
     } catch (_) {
       emit(LoginFailure("Firebase exception"));
+    }
+  }
+
+  Future<void> submitSignOut() async {
+    try {
+      await signOutUseCase.call();
+    } on SocketException catch (e) {
+      
     }
   }
 }
