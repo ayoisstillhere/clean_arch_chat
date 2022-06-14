8235708d508d0ec0940a7173fc8a74b3771952f0
Ayodele Fagbami <afagbami@gmail.com>
2022-06-14 16:07:26 +0100

    created sign out function

--- lib/features/chatroom/domain/repositories/firebase_repository.dart
+++ lib/features/chatroom/domain/repositories/firebase_repository.dart
@@ -5,6 +5,7 @@ abstract class FirebaseRepository {
   Future<void> signUp(String email, String password);
   Future<void> signIn(String email, String password);
   Future<bool> isSignIn();
+  Future<void> signOut();
   Future<String> getCurrentUid();
   Future<void> getCreateCurrentUser(
       String email, String name, String profileUrl);
