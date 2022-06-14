6e8dc62d889576f00100e0cb21d83abf9b806eee
Ayodele Fagbami <afagbami@gmail.com>
2022-06-14 12:05:00 +0100

    set up communication bloc

--- lib/features/chatroom/presentation/bloc/communication/communication_cubit.dart
+++ lib/features/chatroom/presentation/bloc/communication/communication_cubit.dart
@@ -1,8 +1,34 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../../domain/entities/text_message_entity.dart';
+import '../../../domain/usecases/get_messages_usecase.dart';
+import '../../../domain/usecases/send_text_message_usecase.dart';
 
 part 'communication_state.dart';
 
 class CommunicationCubit extends Cubit<CommunicationState> {
-  CommunicationCubit() : super(CommunicationInitial());
+  final GetMessagesUsecase getMessagesUseCase;
+  final SendTextMessageUseCase sendTextMessageUseCase;
+  CommunicationCubit({
+    required this.getMessagesUseCase,
+    required this.sendTextMessageUseCase,
+  }) : super(CommunicationInitial());
+
+  Future<void> sendTextMsg({required TextMessageEntity textMessage}) async {
+    try {
+      await sendTextMessageUseCase.call(textMessage);
+    } on SocketException catch (_) {}
+  }
+
+  Future<void> getTextMessages() async {
+    try {
+      final messages = getMessagesUseCase.call();
+      messages.listen((msg) {
+        emit(CommunicationLoaded(messages: msg));
+      });
+    } on SocketException catch (_) {}
+  }
 }
--- lib/features/chatroom/presentation/bloc/communication/communication_state.dart
+++ lib/features/chatroom/presentation/bloc/communication/communication_state.dart
@@ -2,9 +2,23 @@ part of 'communication_cubit.dart';
 
 abstract class CommunicationState extends Equatable {
   const CommunicationState();
+}
 
+class CommunicationInitial extends CommunicationState {
   @override
   List<Object> get props => [];
 }
 
-class CommunicationInitial extends CommunicationState {}
+class CommunicationLoading extends CommunicationState {
+  @override
+  List<Object?> get props => [];
+}
+
+class CommunicationLoaded extends CommunicationState {
+  final List<TextMessageEntity> messages;
+  CommunicationLoaded({
+    required this.messages,
+  });
+  @override
+  List<Object?> get props => [messages];
+}
